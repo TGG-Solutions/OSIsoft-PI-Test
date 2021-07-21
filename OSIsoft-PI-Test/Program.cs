9001eb9d5478c1ab781c8ca2edffb7fec010de87
Rhys Kirk <rhys@tgg-solutions.com>
2021-07-21 15:14:16 +0100

    Some extra bits

--- OSIsoft-PI-Test/Program.cs
+++ OSIsoft-PI-Test/Program.cs
@@ -13,11 +13,11 @@ namespace OSIsoft_PI_Test
     class Program
     {
 
-        private static string piSystemName = "jupiter001";
+        private static string piSystemName = "JUPITER001";
         private static string piSystemDatabase = "iON";
-        private static string piSystemUser = "";
-        private static string piSystemPass = "";
-        private static string piSystemDomain = "";
+        private static string piSystemUser = "rkirk.adm";
+        private static string piSystemPass = "GXj2N7ee6J7TkPdK";
+        private static string piSystemDomain = "tgg";
 
         static void Main(string[] args)
         {
@@ -46,6 +46,46 @@ namespace OSIsoft_PI_Test
                     Console.WriteLine($"Connected to AF Database '{afDatabase.Name}' (GUID='{afDatabase.ID}')");
 
                     // Do stuff here...
+                    object sysCookie = piSystem.GetFindChangedItemsCookie(searchSandbox: false);
+
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Console.WriteLine("Checking for changes...");
+
+                        List<AFChangeInfo> list = new List<AFChangeInfo>();
+                        int resultsPerPage = 1000;
+                        while (true)
+                        {
+                            var results = piSystem.FindChangedItems(false, true, resultsPerPage, sysCookie, out sysCookie);
+                            if ((results?.Count ?? 0) == 0) break;
+                            list.AddRange(results);
+                        }
+
+                        // Refresh objects that have been changed.
+                        AFChangeInfo.Refresh(piSystem, list);
+
+                        // Find the objects that have been changed.
+                        foreach (AFChangeInfo info in list)
+                        {
+                            try
+                            {
+                                AFObject myObj = info.FindObject(piSystem, true);
+                                Console.WriteLine("Found changed object: {0}", myObj.ToString());
+                                Console.WriteLine("Identity: " + myObj.Identity.ToString());
+                                Console.WriteLine("Action = " + info.Action);
+                                Console.WriteLine("Change Time = " + info.ChangeTime);
+                                Console.WriteLine("Value Updated = " + info.IsValueUpdate);
+                                Console.WriteLine("Parent ID = " + info.ParentID);
+                                Console.WriteLine("Parent = " + ((info.ParentID == Guid.Empty) ? "NONE" : AFElement.FindElement(piSystem, info.ParentID).GetPath()));
+                                Console.WriteLine(" ");
+                            }
+                            catch { }
+                        }
+
+                        System.Threading.Thread.Sleep(5000);
+                    }
+
+                    
                 }
                 else
                 {
